e90fea894b10673647b772023fb3be4483956156
Dan <daniellerma05218@gmail.com>
2021-11-19 17:11:37 +0100

    Main

--- main.c
+++ main.c
@@ -9,153 +9,156 @@ int	main(int argc, char **argv)
 
 	(void)argc;
 
-	if (strcmp(argv[1], "string") == 0)
+	if (argc == 2)
 	{
-		printf("\n\nORIGINAL: \n");
-		result = printf("SOLO UN STRING.");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("SOLO UN STRING.");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("ORIGINAL: \n");
-		result = printf("CHAR RAROS: ¢@#|∞≠çñäêó$&¿?¡!");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("CHAR RAROS: ¢@#|∞≠çñäêó$&¿?¡!");
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%c") == 0)
-	{
-		printf("ORIGINAL: \n");
-		result = printf("%c %c %c %c %c %c", 'a', 's', '2', 'y', '=', '?');
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%c %c %c %c %c %c", 'a', 's', '2', 'y', '=', '?');
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%d") == 0)
-	{
-		printf("ORIGINAL: \n");
-		result = printf("%d %d %d %d %d %d %d", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%d %d %d %d %d %d %d", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%i") == 0)
-	{
-		printf("ORIGINAL: \n");
-		result = printf("%i %i %i %i %i %i %i", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%i %i %i %i %i %i %i", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%u") == 0)
-	{
-		printf("ORIGINAL: \n");
-		result = printf("%u %u %u %u %u %u %u", -1, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%u %u %u %u %u %u %u", -1, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%x") == 0)
-	{
-		printf("ORIGINAL: \n");
-		result = printf("%x %x %x %x %x %x %x", 0, -1 , 1, -1000000000, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%x %x %x %x %x %x %x", 0, -1 , 1, -1000000000, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-		/////////////////////////////////////
-		printf("ORIGINAL: \n");
-		result = printf( " %x ", 0);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf(" %x ", 0);
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%X") == 0)
-	{
-		printf("ORIGINAL: \n");
-		result = printf("%X %X %X %X %X %X %X\n", 16, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%X %X %X %X %X %X %X\n", 16, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%s") == 0)
-	{
-		printf("ORIGINAL: \n");
-		result = printf("%s", "Texto simple");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%s", "Texto simple");
-		printf("\nNUM: %d\n\n", result);
-		///////////////////////////////////
-		printf("\nORIGINAL: \n");
-		result = printf("%s %s", "Texto simple1", "Texto simple2");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%s %s", "Texto simple1", "Texto simple2");
-		printf("\nNUM: %d\n\n", result);
-		///////////////////////////////////
-		printf("\nORIGINAL: \n");
-		result = printf("%s %s", "Caracteres raros", "¢@#|∞≠çñäêó$&¿?¡!");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%s %s", "Caracteres raros", "¢@#|∞≠çñäêó$&¿?¡!");
-		printf("\nNUM: %d\n\n", result);
-		///////////////////////////////////
-		printf("\nORIGINAL: \n");
-		result = printf("NULL %s NULL", NULL);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("NULL %s NULL", NULL);
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%%") == 0)
-	{
-		printf("\nORIGINAL: \n");
-		result = printf("%% %% %% %% %%");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%% %% %% %% %%");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("\nORIGINAL: \n");
-		result = printf("%%%%%%%%%%%%%%%%%%%%");
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%%%%%%%%%%%%%%%%%%%%");
-		printf("\nNUM: %d\n\n", result);
-	}
-	else if (strcmp(argv[1], "%p") == 0)
-	{
-		printf("\nORIGINAL: \n");
-		result = printf("%p %p %p %p %p %p %p", (void *)-1, (void *)32, (void *)-98, (void *)548987, (void *)2147483647, (void *)-1, (void *)1);
-		printf("\nNUM: %d\n\n", result);
-		////////////////////////////////////
-		printf("FUNCION: \n");
-		result = ft_printf("%p %p %p %p %p %p %p", (void *)-1, (void *)32, (void *)-98, (void *)548987, (void *)2147483647, (void *)-1, (void *)1);
-		printf("\nNUM: %d\n\n", result);
+		if (strcmp(argv[1], "string") == 0)
+		{
+			printf("\n\nORIGINAL: \n");
+			result = printf("SOLO UN STRING.");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("SOLO UN STRING.");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("ORIGINAL: \n");
+			result = printf("CHAR RAROS: ¢@#|∞≠çñäêó$&¿?¡!");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("CHAR RAROS: ¢@#|∞≠çñäêó$&¿?¡!");
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%c") == 0)
+		{
+			printf("ORIGINAL: \n");
+			result = printf("%c %c %c %c %c %c", 'a', 's', '2', 'y', '=', '?');
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%c %c %c %c %c %c", 'a', 's', '2', 'y', '=', '?');
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%d") == 0)
+		{
+			printf("ORIGINAL: \n");
+			result = printf("%d %d %d %d %d %d %d", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%d %d %d %d %d %d %d", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%i") == 0)
+		{
+			printf("ORIGINAL: \n");
+			result = printf("%i %i %i %i %i %i %i", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%i %i %i %i %i %i %i", 1, 98 , -45, 1245648, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%u") == 0)
+		{
+			printf("ORIGINAL: \n");
+			result = printf("%u %u %u %u %u %u %u", -1, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%u %u %u %u %u %u %u", -1, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%x") == 0)
+		{
+			printf("ORIGINAL: \n");
+			result = printf("%x %x %x %x %x %x %x", 0, -1 , 1, -1000000000, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%x %x %x %x %x %x %x", 0, -1 , 1, -1000000000, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+			/////////////////////////////////////
+			printf("ORIGINAL: \n");
+			result = printf( " %x ", 0);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf(" %x ", 0);
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%X") == 0)
+		{
+			printf("ORIGINAL: \n");
+			result = printf("%X %X %X %X %X %X %X\n", 16, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%X %X %X %X %X %X %X\n", 16, 98 , -4545, -1000000000, -2147483647, 2147483647, 0);
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%s") == 0)
+		{
+			printf("ORIGINAL: \n");
+			result = printf("%s", "Texto simple");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%s", "Texto simple");
+			printf("\nNUM: %d\n\n", result);
+			///////////////////////////////////
+			printf("\nORIGINAL: \n");
+			result = printf("%s %s", "Texto simple1", "Texto simple2");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%s %s", "Texto simple1", "Texto simple2");
+			printf("\nNUM: %d\n\n", result);
+			///////////////////////////////////
+			printf("\nORIGINAL: \n");
+			result = printf("%s %s", "Caracteres raros", "¢@#|∞≠çñäêó$&¿?¡!");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%s %s", "Caracteres raros", "¢@#|∞≠çñäêó$&¿?¡!");
+			printf("\nNUM: %d\n\n", result);
+			///////////////////////////////////
+			printf("\nORIGINAL: \n");
+			result = printf("NULL %s NULL", NULL);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("NULL %s NULL", NULL);
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%%") == 0)
+		{
+			printf("\nORIGINAL: \n");
+			result = printf("%% %% %% %% %%");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%% %% %% %% %%");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("\nORIGINAL: \n");
+			result = printf("%%%%%%%%%%%%%%%%%%%%");
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%%%%%%%%%%%%%%%%%%%%");
+			printf("\nNUM: %d\n\n", result);
+		}
+		else if (strcmp(argv[1], "%p") == 0)
+		{
+			printf("\nORIGINAL: \n");
+			result = printf("%p %p %p %p %p %p %p", (void *)-1, (void *)32, (void *)-98, (void *)548987, (void *)2147483647, (void *)-54465, (void *)1);
+			printf("\nNUM: %d\n\n", result);
+			////////////////////////////////////
+			printf("FUNCION: \n");
+			result = ft_printf("%p %p %p %p %p %p %p", (void *)-1, (void *)32, (void *)-98, (void *)548987, (void *)2147483647, (void *)-54465, (void *)1);
+			printf("\nNUM: %d\n\n", result);
+		}
 	}
 	system("leaks a.out\n");
 	return (0);
